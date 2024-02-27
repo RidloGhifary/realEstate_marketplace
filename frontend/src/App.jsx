c304c057f034148d44b41ed68ee033915346bcf5
ridho <ridloghfry@gmail.com>
2024-02-27 13:37:35 +0700

    Create pages & routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
-    <div className="flex w-full items-center justify-center rounded bg-blue-300">
-      <h1 className="text-center font-bold text-red-500">hello world</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
   );
 }
 
