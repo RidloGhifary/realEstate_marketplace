7312065a698e00a1fa9848b9ea5abe4abd22f478
ridho <ridloghfry@gmail.com>
2024-02-27 13:20:06 +0700

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex w-full items-center justify-center rounded bg-blue-300">
+      <h1 className="text-center font-bold text-red-500">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
