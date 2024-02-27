aff6562501aa3da1200b1dc2ec934f56a37ababe
RidloGhifary <ridloghfry@gmail.com>
2024-02-27 14:53:09 +0700

    Connecting to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv/config");
 
 const PORT = process.env.PORT;
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log("Database connected"))
+  .catch(() => console.log("Database error"));
 
 app.use(express.json());
 app.use(cors());
-
-const app = express();
 
 app.listen(PORT, () => console.log("Connected on port:", PORT));
